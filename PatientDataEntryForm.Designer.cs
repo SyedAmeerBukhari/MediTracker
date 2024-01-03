28445511bb295e812bd8b95ffdd9d004174f639e
Syed Ameer <75444035+Codecraft-co@users.noreply.github.com>
2024-01-03 21:26:18 +0500

    Data entry Form Added

---
+++ PatientDataEntryForm.Designer.cs
@@ -0,0 +1,488 @@
+﻿namespace MediTracker
+{
+    partial class PatientDataEntryForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            patientDataEntryTabControl = new TabControl();
+            patientBiographyTabPage = new TabPage();
+            button1 = new Button();
+            textBox9 = new TextBox();
+            label13 = new Label();
+            textBox10 = new TextBox();
+            label14 = new Label();
+            textBox7 = new TextBox();
+            label10 = new Label();
+            textBox8 = new TextBox();
+            label11 = new Label();
+            label12 = new Label();
+            textBox6 = new TextBox();
+            label9 = new Label();
+            textBox5 = new TextBox();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            medicalHistoryTabPage = new TabPage();
+            labHistoryTabPage = new TabPage();
+            radiologyTabPage1 = new TabPage();
+            proceduresTabPage = new TabPage();
+            referTabPage = new TabPage();
+            cunsultentOpinionTabPage = new TabPage();
+            caseSummaryTabPage = new TabPage();
+            panel1 = new Panel();
+            label5 = new Label();
+            textBox11 = new TextBox();
+            patientDataEntryTabControl.SuspendLayout();
+            patientBiographyTabPage.SuspendLayout();
+            SuspendLayout();
+            // 
+            // patientDataEntryTabControl
+            // 
+            patientDataEntryTabControl.Alignment = TabAlignment.Left;
+            patientDataEntryTabControl.Controls.Add(patientBiographyTabPage);
+            patientDataEntryTabControl.Controls.Add(medicalHistoryTabPage);
+            patientDataEntryTabControl.Controls.Add(labHistoryTabPage);
+            patientDataEntryTabControl.Controls.Add(radiologyTabPage1);
+            patientDataEntryTabControl.Controls.Add(proceduresTabPage);
+            patientDataEntryTabControl.Controls.Add(referTabPage);
+            patientDataEntryTabControl.Controls.Add(cunsultentOpinionTabPage);
+            patientDataEntryTabControl.Controls.Add(caseSummaryTabPage);
+            patientDataEntryTabControl.DrawMode = TabDrawMode.OwnerDrawFixed;
+            patientDataEntryTabControl.Font = new Font("Microsoft YaHei", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            patientDataEntryTabControl.ItemSize = new Size(35, 120);
+            patientDataEntryTabControl.Location = new Point(-1, 96);
+            patientDataEntryTabControl.Margin = new Padding(0);
+            patientDataEntryTabControl.Multiline = true;
+            patientDataEntryTabControl.Name = "patientDataEntryTabControl";
+            patientDataEntryTabControl.Padding = new Point(0, 0);
+            patientDataEntryTabControl.RightToLeft = RightToLeft.No;
+            patientDataEntryTabControl.SelectedIndex = 0;
+            patientDataEntryTabControl.ShowToolTips = true;
+            patientDataEntryTabControl.Size = new Size(799, 357);
+            patientDataEntryTabControl.SizeMode = TabSizeMode.Fixed;
+            patientDataEntryTabControl.TabIndex = 0;
+            patientDataEntryTabControl.TabStop = false;
+            // 
+            // patientBiographyTabPage
+            // 
+            patientBiographyTabPage.BackColor = Color.LightSteelBlue;
+            patientBiographyTabPage.Controls.Add(label5);
+            patientBiographyTabPage.Controls.Add(textBox11);
+            patientBiographyTabPage.Controls.Add(button1);
+            patientBiographyTabPage.Controls.Add(textBox9);
+            patientBiographyTabPage.Controls.Add(label13);
+            patientBiographyTabPage.Controls.Add(textBox10);
+            patientBiographyTabPage.Controls.Add(label14);
+            patientBiographyTabPage.Controls.Add(textBox7);
+            patientBiographyTabPage.Controls.Add(label10);
+            patientBiographyTabPage.Controls.Add(textBox8);
+            patientBiographyTabPage.Controls.Add(label11);
+            patientBiographyTabPage.Controls.Add(label12);
+            patientBiographyTabPage.Controls.Add(textBox6);
+            patientBiographyTabPage.Controls.Add(label9);
+            patientBiographyTabPage.Controls.Add(textBox5);
+            patientBiographyTabPage.Controls.Add(label8);
+            patientBiographyTabPage.Controls.Add(label7);
+            patientBiographyTabPage.Controls.Add(label6);
+            patientBiographyTabPage.Controls.Add(textBox1);
+            patientBiographyTabPage.Controls.Add(label1);
+            patientBiographyTabPage.Controls.Add(comboBox1);
+            patientBiographyTabPage.Controls.Add(textBox4);
+            patientBiographyTabPage.Controls.Add(label4);
+            patientBiographyTabPage.Controls.Add(textBox3);
+            patientBiographyTabPage.Controls.Add(label3);
+            patientBiographyTabPage.Controls.Add(textBox2);
+            patientBiographyTabPage.Controls.Add(label2);
+            patientBiographyTabPage.ForeColor = Color.Black;
+            patientBiographyTabPage.ImeMode = ImeMode.NoControl;
+            patientBiographyTabPage.Location = new Point(124, 4);
+            patientBiographyTabPage.Name = "patientBiographyTabPage";
+            patientBiographyTabPage.Padding = new Padding(3);
+            patientBiographyTabPage.Size = new Size(671, 349);
+            patientBiographyTabPage.TabIndex = 0;
+            patientBiographyTabPage.Text = "Patient Biography";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonFace;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(665, 367);
+            button1.Name = "button1";
+            button1.Size = new Size(111, 44);
+            button1.TabIndex = 28;
+            button1.Text = "Save ";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(275, 320);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 23);
+            textBox9.TabIndex = 27;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(275, 302);
+            label13.Name = "label13";
+            label13.Size = new Size(79, 17);
+            label13.TabIndex = 26;
+            label13.Text = "Relationship";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(275, 219);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(100, 23);
+            textBox10.TabIndex = 25;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(275, 201);
+            label14.Name = "label14";
+            label14.Size = new Size(91, 17);
+            label14.TabIndex = 24;
+            label14.Text = "Email Adderss";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(158, 320);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 23;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(158, 302);
+            label10.Name = "label10";
+            label10.Size = new Size(104, 17);
+            label10.TabIndex = 22;
+            label10.Text = "Contact Number";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(38, 320);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 21;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(38, 302);
+            label11.Name = "label11";
+            label11.Size = new Size(43, 17);
+            label11.TabIndex = 20;
+            label11.Text = "Name";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(38, 264);
+            label12.Name = "label12";
+            label12.Size = new Size(192, 17);
+            label12.TabIndex = 19;
+            label12.Text = "Emergency Contact Information";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(158, 219);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 18;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(158, 201);
+            label9.Name = "label9";
+            label9.Size = new Size(104, 17);
+            label9.TabIndex = 17;
+            label9.Text = "Contact Number";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(38, 219);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 16;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(38, 201);
+            label8.Name = "label8";
+            label8.Size = new Size(56, 17);
+            label8.TabIndex = 15;
+            label8.Text = "Adderss";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(38, 163);
+            label7.Name = "label7";
+            label7.Size = new Size(124, 17);
+            label7.TabIndex = 14;
+            label7.Text = "Contact Information";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(160, 86);
+            label6.Name = "label6";
+            label6.Size = new Size(84, 17);
+            label6.TabIndex = 13;
+            label6.Text = "Blood Group";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(160, 104);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 86);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 17);
+            label1.TabIndex = 11;
+            label1.Text = "Age";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Male", "Female", "Other" });
+            comboBox1.Location = new Point(343, 35);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 25);
+            comboBox1.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(144, 35);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(144, 17);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 17);
+            label4.TabIndex = 6;
+            label4.Text = "Last Name";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(38, 104);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(343, 17);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 17);
+            label3.TabIndex = 4;
+            label3.Text = "Gender";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(38, 35);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(38, 17);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 17);
+            label2.TabIndex = 2;
+            label2.Text = "First Name";
+            // 
+            // medicalHistoryTabPage
+            // 
+            medicalHistoryTabPage.BackColor = Color.LightSteelBlue;
+            medicalHistoryTabPage.Location = new Point(124, 4);
+            medicalHistoryTabPage.Name = "medicalHistoryTabPage";
+            medicalHistoryTabPage.Padding = new Padding(3);
+            medicalHistoryTabPage.Size = new Size(671, 349);
+            medicalHistoryTabPage.TabIndex = 1;
+            medicalHistoryTabPage.Text = "Medical History";
+            // 
+            // labHistoryTabPage
+            // 
+            labHistoryTabPage.Location = new Point(124, 4);
+            labHistoryTabPage.Name = "labHistoryTabPage";
+            labHistoryTabPage.Size = new Size(671, 349);
+            labHistoryTabPage.TabIndex = 2;
+            labHistoryTabPage.Text = "Lab History";
+            labHistoryTabPage.UseVisualStyleBackColor = true;
+            // 
+            // radiologyTabPage1
+            // 
+            radiologyTabPage1.Location = new Point(124, 4);
+            radiologyTabPage1.Name = "radiologyTabPage1";
+            radiologyTabPage1.Size = new Size(671, 349);
+            radiologyTabPage1.TabIndex = 3;
+            radiologyTabPage1.Text = "Radiology";
+            radiologyTabPage1.UseVisualStyleBackColor = true;
+            // 
+            // proceduresTabPage
+            // 
+            proceduresTabPage.Location = new Point(124, 4);
+            proceduresTabPage.Name = "proceduresTabPage";
+            proceduresTabPage.Size = new Size(671, 349);
+            proceduresTabPage.TabIndex = 4;
+            proceduresTabPage.Text = "Procedures / Surgury";
+            proceduresTabPage.UseVisualStyleBackColor = true;
+            // 
+            // referTabPage
+            // 
+            referTabPage.Location = new Point(124, 4);
+            referTabPage.Name = "referTabPage";
+            referTabPage.Size = new Size(671, 349);
+            referTabPage.TabIndex = 5;
+            referTabPage.Text = "Refer";
+            referTabPage.UseVisualStyleBackColor = true;
+            // 
+            // cunsultentOpinionTabPage
+            // 
+            cunsultentOpinionTabPage.Location = new Point(124, 4);
+            cunsultentOpinionTabPage.Name = "cunsultentOpinionTabPage";
+            cunsultentOpinionTabPage.Size = new Size(671, 349);
+            cunsultentOpinionTabPage.TabIndex = 6;
+            cunsultentOpinionTabPage.Text = "Cunsultent Opinion";
+            cunsultentOpinionTabPage.UseVisualStyleBackColor = true;
+            // 
+            // caseSummaryTabPage
+            // 
+            caseSummaryTabPage.Location = new Point(124, 4);
+            caseSummaryTabPage.Name = "caseSummaryTabPage";
+            caseSummaryTabPage.Size = new Size(671, 349);
+            caseSummaryTabPage.TabIndex = 7;
+            caseSummaryTabPage.Text = "Case Summary";
+            caseSummaryTabPage.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 93);
+            panel1.TabIndex = 1;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(275, 86);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 17);
+            label5.TabIndex = 30;
+            label5.Text = "CNIC";
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(275, 104);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(100, 23);
+            textBox11.TabIndex = 29;
+            // 
+            // PatientDataEntryForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(patientDataEntryTabControl);
+            Name = "PatientDataEntryForm";
+            Text = "Form1";
+            patientDataEntryTabControl.ResumeLayout(false);
+            patientBiographyTabPage.ResumeLayout(false);
+            patientBiographyTabPage.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl patientDataEntryTabControl;
+        private TabPage patientBiographyTabPage;
+        private TabPage medicalHistoryTabPage;
+        private TabPage labHistoryTabPage;
+        private TabPage radiologyTabPage1;
+        private TabPage proceduresTabPage;
+        private TabPage referTabPage;
+        private TabPage cunsultentOpinionTabPage;
+        private TabPage caseSummaryTabPage;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Label label6;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox6;
+        private Label label9;
+        private TextBox textBox5;
+        private Label label8;
+        private Label label7;
+        private TextBox textBox9;
+        private Label label13;
+        private TextBox textBox10;
+        private Label label14;
+        private TextBox textBox7;
+        private Label label10;
+        private TextBox textBox8;
+        private Label label11;
+        private Label label12;
+        private Button button1;
+        private Panel panel1;
+        private Label label5;
+        private TextBox textBox11;
+    }
+}
---
+++ PatientDataEntryForm.cs
@@ -0,0 +1,46 @@
+using System.Windows.Forms;
+
+namespace MediTracker
+{
+    public partial class PatientDataEntryForm : Form
+    {
+        public PatientDataEntryForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void patientDataEntryTabControl_DrawItem(Object sender, System.Windows.Forms.DrawItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Brush _textBrush;
+
+            // Get the item from the collection.
+            TabPage _tabPage = patientDataEntryTabControl.TabPages[e.Index];
+
+            // Get the real bounds for the tab rectangle.
+            Rectangle _tabBounds = patientDataEntryTabControl.GetTabRect(e.Index);
+
+            if (e.State == DrawItemState.Selected)
+            {
+
+                // Draw a different background color, and don't paint a focus rectangle.
+                _textBrush = new SolidBrush(Color.Black);
+                g.FillRectangle(Brushes.Gray, e.Bounds);
+            }
+            else
+            {
+                _textBrush = new System.Drawing.SolidBrush(e.ForeColor);
+                e.DrawBackground();
+            }
+
+            // Use our own font.
+            Font _tabFont = new Font("Arial", 10.0f, FontStyle.Bold, GraphicsUnit.Pixel);
+
+            // Draw string. Center the text.
+            StringFormat _stringFlags = new StringFormat();
+            _stringFlags.Alignment = StringAlignment.Center;
+            _stringFlags.LineAlignment = StringAlignment.Center;
+            g.DrawString(_tabPage.Text, _tabFont, _textBrush, _tabBounds, new StringFormat(_stringFlags));
+        }
+    }
+}
