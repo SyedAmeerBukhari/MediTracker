b978f12216bd57ad54bc6e16aa78706b519eace4
Syed Ameer <75444035+Codecraft-co@users.noreply.github.com>
2024-01-10 23:07:35 +0500

    DB file Added

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Patients]
+(
+	[PatientId] INT NOT NULL PRIMARY KEY, 
+    [fName] NVARCHAR(50) NULL, 
+    [lName] NVARCHAR(50) NULL, 
+    [Gender] NCHAR(10) NULL, 
+    [Age] INT NULL, 
+    [BloodGroup] NCHAR(10) NULL, 
+    [CNIC] DECIMAL NULL
+)
